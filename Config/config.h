de6c640d1a4b1e887ba6e2187883b159f8f8b650
Elliot <elliotl@torpor.com>
2020-01-25 15:13:22 -0800

    Building on Mac Mojave. Installed homebrew. brew install gcc  ./configure F77=gfortran

---
+++ Config/config.h
@@ -0,0 +1,93 @@
+/* Config/config.h.  Generated from config.h.in by configure.  */
+/* Config/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Compile leading two-loop SUSY threshold corrections to fermionic third
+   generation Yukawa couplings */
+/* #undef COMPILE_TWO_LOOP_GAUGE_YUKAWA */
+
+/* Enable GSL routines */
+/* #undef ENABLE_GSL */
+
+/* Include 3-loop Higgs mass contributions from Himalaya */
+/* #undef ENABLE_HIMALAYA */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <Eigen/Dense> header file. */
+/* #undef HAVE_EIGEN_DENSE */
+
+/* Define to 1 if you have the <gsl/gsl_deriv.h> header file. */
+/* #undef HAVE_GSL_GSL_DERIV_H */
+
+/* Define to 1 if you have the <gsl/gsl_multiroots.h> header file. */
+/* #undef HAVE_GSL_GSL_MULTIROOTS_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `gsl' library (-lgsl). */
+/* #undef HAVE_LIBGSL */
+
+/* Define to 1 if you have the `gslcblas' library (-lgslcblas). */
+/* #undef HAVE_LIBGSLCBLAS */
+
+/* Define to 1 if you have the `m' library (-lm). */
+/* #undef HAVE_LIBM */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Looptools evaluation of Pasarino-Veltman functions */
+/* #undef LOOPTOOLS */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "softsusy"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "B.C.Allanach@damtp.cam.ac.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "softsusy"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "softsusy 4.1.8"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "softsusy"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://softsusy.hepforge.org/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "4.1.8"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "4.1.8"
